abdc5792b70da2821d65fbb82f316101faa73349
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2016-01-05 16:26:10 +0100

    add init weights

--- include/vector_ann.h
+++ include/vector_ann.h
@@ -3,3 +3,30 @@
 #include <math.h>
 #include <inttypes.h>
 #include <volk/volk.h>
+
+/*************************
+ * ANN definition struct *
+ *************************/
+struct ann{
+    // Number of layers
+    size_t    num_layers;
+
+    // Pointer to array with number of nodes per layer
+    size_t*   num_nodes;
+
+    // Pointer to weights matrix with indices [layer][neuron this layer][neuron next layer]
+    float***  weights;
+};
+
+/*******************************
+ * Get random number in [-1,1] *
+ *******************************/
+float rndm(){
+    float val = 2.0*rand()/(float)RAND_MAX - 1.0;
+    return val;
+}
+
+/***************************************************************************
+ * Allocate memory for weights and init them with random numbers in [-1,1] *
+ ***************************************************************************/
+void init_weights(struct ann*);
--- tests/qa_init.c
+++ tests/qa_init.c
@@ -1,6 +1,17 @@
 #include <vector_ann.h>
 
 int main(){
+    // Init neural network struct
+    struct ann net;
+    net.num_layers = 3;
+    net.num_nodes = (size_t*) malloc(sizeof(size_t)*net.num_layers);
+    net.num_nodes[0] = 2;
+    net.num_nodes[1] = 3;
+    net.num_nodes[2] = 1;
+
+    // Init weights
+    init_weights(&net);
+
     // Test ends successfully
     return 0;
 }
